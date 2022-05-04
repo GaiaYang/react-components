31419b664aafaa6ea6b8207f97a8f9bccb3b2354
Gaia <st90070279279@gmail.com>
2022-05-04 19:27:20 +0800

    新增footer、空白資料佔位顯示

--- mui/CombinedTable.js
+++ mui/CombinedTable.js
@@ -4,6 +4,7 @@ import {
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
   TableRow,
 } from "@mui/material";
@@ -16,6 +17,7 @@ import {
  * @typedef {import('@mui/material').TableContainerProps} TableContainerProps
  * @typedef {import('@mui/material').TableHeadProps} TableHeadProps
  * @typedef {import('@mui/material').TableRowProps} TableRowProps
+ * @typedef {import('@mui/material').TableFooterProps} TableFooterProps
  */
 /**
  * @typedef {Omit<TableRowProps, 'children'> & {children: TableCellProps[]}} tableItem
@@ -25,12 +27,15 @@ import {
  * @typedef CombinedTableProps - 組件型別
  * @property {Array<ItemT>} data - 表格資料
  * @property {tableItem} head - 表格標頭
+ * @property {tableItem} footer - 表格尾部
  * @property {(item: ItemT, index: number) => tableItem} body - 資料渲染
+ * @property {TableCellProps} emptyItem - 空資料時渲染
  * @property {(item: ItemT, index: number) => string} keyExtractor - 生成陣列key
  * @property {Omit<TableProps, 'children'>} tableProps - table組件屬性
  * @property {Omit<TableContainerProps, 'children'>} tableContainerProps - tableContainer組件屬性
  * @property {Omit<TableHeadProps, 'children'>} tableHeadProps - tableHeadProps組件屬性
  * @property {Omit<TableBodyProps, 'children'>} tableBodyProps - tableBodyProps組件屬性
+ * @property {Omit<TableFooterProps, 'children'>} tableFooterProps - tableFooterProps組件屬性
  */
 /**
  * @template T - 泛型對應到data的位置
@@ -41,54 +46,74 @@ export default function CombinedTable({
   data = [],
   head,
   body,
+  footer,
   keyExtractor,
   tableProps,
   tableContainerProps,
   tableHeadProps,
   tableBodyProps,
+  tableFooterProps,
+  emptyItem,
 }) {
-  // NOTE 渲染標題
-  function renderHead() {
-    if (head) {
-      const { children, ...otherHead } = head;
-      return (
-        <TableRow {...otherHead}>
-          {arrayDebug(children).map((item, index) => (
-            <TableCell {...item} key={index} />
-          ))}
-        </TableRow>
-      );
-    }
-    return null;
-  }
   // NOTE 渲染資料本體
-  function renderBody() {
-    if (typeof body === "function") {
-      return arrayDebug(data).map((item, index) => {
-        const { children, ...otherBody } = body(item, index);
-        const key =
-          typeof keyExtractor === "function"
-            ? keyExtractor(item, index)
-            : index;
+  function _renderBody() {
+    const renderData = arrayDebug(data);
+    if (renderData.length > 0) {
+      if (typeof body === "function") {
+        return renderData.map((item, index) => {
+          const { children, ...otherBody } = body(item, index);
+          const key =
+            typeof keyExtractor === "function"
+              ? keyExtractor(item, index)
+              : index;
+          return (
+            <TableRow {...otherBody} key={key}>
+              {arrayDebug(children).map((child, childIndex) => (
+                <TableCell {...child} key={childIndex} />
+              ))}
+            </TableRow>
+          );
+        });
+      } else {
+        return null;
+      }
+    } else {
+      // NOTE 沒有資料時可選顯示
+      if (head && emptyItem) {
+        const { children } = head;
         return (
-          <TableRow {...otherBody} key={key}>
-            {arrayDebug(children).map((child, childIndex) => (
-              <TableCell {...child} key={childIndex} />
-            ))}
+          <TableRow>
+            <TableCell colSpan={arrayDebug(children).length} {...emptyItem} />
           </TableRow>
         );
-      });
+      } else {
+        return null;
+      }
     }
-    return null;
   }
   return (
     <TableContainer {...tableContainerProps}>
       <Table {...tableProps}>
-        <TableHead {...tableHeadProps}>{renderHead()}</TableHead>
-        <TableBody {...tableBodyProps}>{renderBody()}</TableBody>
+        <TableHead {...tableHeadProps}>{_renderStatic(head)}</TableHead>
+        <TableBody {...tableBodyProps}>{_renderBody()}</TableBody>
+        <TableFooter {...tableFooterProps}>{_renderStatic(footer)}</TableFooter>
       </Table>
     </TableContainer>
   );
+}
+function _renderStatic(params) {
+  if (params) {
+    const { children, ...other } = params;
+    return (
+      <TableRow {...other}>
+        {arrayDebug(children).map((item, index) => (
+          <TableCell {...item} key={index} />
+        ))}
+      </TableRow>
+    );
+  } else {
+    return null;
+  }
 }
 // NOTE 預處理判斷是否為陣列
 function arrayDebug(params) {
